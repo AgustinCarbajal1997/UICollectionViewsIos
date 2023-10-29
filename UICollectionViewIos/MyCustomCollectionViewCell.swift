b195766241d947e4bf892f59f12ec3ba15bc4d9b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-28 21:36:42 -0300

    [feature] delegate collection

---
+++ UICollectionViewIos/MyCustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCollectionViewCell.swift
+//  UICollectionViewIos
+//
+//  Created by Agustin Carbajal on 28/10/2023.
+//
+
+import UIKit
+
+class MyCustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
